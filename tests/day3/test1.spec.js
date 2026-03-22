207b60213ca9801808ebfe8c6927eda719cdb2d0
shekharapple16-spec <shekharapple16@gmail.com>
2026-03-22 08:42:00 +0530

    fix: update waitForSelector to use correct selector

--- tests/day3/test1.spec.js
+++ tests/day3/test1.spec.js
@@ -1,26 +1,9 @@
-//1.Use Case: Herokuapp Login Validation
-//
-//Scenario Description:
-//
-//Navigate to the login pag
-//
-//https://the-internet.herokuapp.com/login
-//
-//Username=tomsmith
-//
-//Password=SuperSecretPassword!
-//
-//Fill in the username and password using (id attribute)
-//
-//Click the login button using its ARIA role(getByRole())
-
-
 // @ts-check
 import { test, expect } from '#fixtures';
 
 test('Herokuapp Login Validation', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
-  await page.waitForSelector('#username1', { timeout: 10000 });
+  await page.waitForSelector('#username', { timeout: 10000 });
   await page.fill('#username', 'tomsmith');
   await page.fill('#password', 'SuperSecretPassword!');
   await page.getByRole('button', { name: 'Login' }).click();
