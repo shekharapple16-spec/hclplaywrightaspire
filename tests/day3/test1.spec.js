0bf86cb6956c2cfe88286f04e7f6afed4db28337
shekharapple16-spec <shekharapple16@gmail.com>
2026-03-22 09:44:12 +0530

    Updated waitForSelector to use correct id

--- tests/day3/test1.spec.js
+++ tests/day3/test1.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '#fixtures';
 
 test('Herokuapp Login Validation', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
-  await page.waitForSelector('#username12', { timeout: 10000 });
+  await page.waitForSelector('#username', { timeout: 10000 });
   await page.fill('#username', 'tomsmith');
   await page.fill('#password', 'SuperSecretPassword!');
   await page.getByRole('button', { name: 'Login' }).click();
