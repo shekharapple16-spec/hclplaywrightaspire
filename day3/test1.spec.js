c1e43d5a35276cb5b8745d72a4fb8f97693c8647
shekharapple16-spec <shekharapple16@gmail.com>
2026-03-22 07:58:53 +0530

    Added basic Playwright test example

---
+++ day3/test1.spec.js
@@ -0,0 +1,5 @@
+const { test, expect } = require('@playwright/test');
+test('example test', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toContainText('Example Domain');
+});
