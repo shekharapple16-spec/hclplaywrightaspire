4e812e98a4812c1ee2e3586702f2e111fe629bf8
shekharapple16-spec <shekharapple16@gmail.com>
2026-03-24 12:02:08 +0530

    add wait for navigation after clicking login button

--- tests/day3/test2.spec.js
+++ tests/day3/test2.spec.js
@@ -1,19 +1,3 @@
-//2. OrangeHRM Login Validation
-//
-//https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
-//
-//User name:Admin
-//
-//password:admin123
-//
-//
-//Scenario Description:
-//
-//Navigate to the OrangeHRM login page
-//
-//Enter valid credentials(getByPlaceholder())
-//
-//Click the login button(getByRole())
 // @ts-check
 import { test, expect } from '../../fixtures.js';
 test('OrangeHRM Login Validation', async ({ page }) => {
@@ -21,5 +5,6 @@ test('OrangeHRM Login Validation', async ({ page }) => {
   await page.getByPlaceholder('Username').fill('Admin');
   await page.getByPlaceholder('Password').fill('admin123');
   await page.getByRole('button', { name: 'Login' }).click();
+  await page.waitForNavigation();
   await expect(page).toHaveURL(/dashboard/);
 });
