d39675786c8b8bf4e17eb11dc8286271a4b5dca5
shekharapple16-spec <shekharapple16@gmail.com>
2026-03-26 09:53:34 +0530

    Update test1.spec.js

--- tests/day3/test1.spec.js
+++ tests/day3/test1.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Herokuapp Login Validation', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
   await page.waitForSelector('#username', { timeout: 10000 });
-  await page.fill('#username', 'tomsmith');
+  await page.fill('#username123', 'tomsmith');
   await page.fill('#password', 'SuperSecretPassword!');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.waitForLoadState('networkidle');
